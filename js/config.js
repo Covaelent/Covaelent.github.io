1d43e1a56b687d2ce14b2d60f92cf6b3ec7cffce
Covλlent <lunatsarratanai@gmail.com>
2022-07-06 21:31:37 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "";
+Config.title = "Covλlent";
 
 /**
  * Enable map text in the top left corner of the screen?
@@ -41,7 +41,7 @@ Config.announcementLength = 3000;
  * Image Filename
  * DROP IMAGE IN "images" FOLDER
  */
-Config.backgroundImage = "";
+Config.backgroundImage = "default.jpg";
 
 /**
  * Enable debug messages?
