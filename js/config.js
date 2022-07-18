26f073a24c7a2b28096c6dcda0c315b655883ef9
Covλlent <lunatsarratanai@gmail.com>
2022-07-18 16:38:22 +0100

    Tlou

--- js/config.js
+++ js/config.js
@@ -1,48 +1,119 @@
-// ignore
-var Config = {};
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
 
-/**
- * What should the text in the center of the screen be?
- * if empty it will fill in your Server Name
- */
-Config.title = "Covλlent";
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "";
 
-/**
- * Enable map text in the top left corner of the screen?
- */
-Config.enableMap = true;
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "The Last of Us";
 
-/**
- * Enable steamId text in the top right corner of the screen?
- */
-Config.enableSteamID = true;
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = true;
 
-/**
- * Enable announcements?
- */
-Config.enableAnnouncements = true;
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
 
-/**
- * What messages do you want to show up?
- * only works if enableAnnouncements = true
- */
-Config.announceMessages = [
-  "Enjoy your stay!"
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/tlou.webm";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/touhou01.jpg",
+	"backgrounds/images/touhou02.jpg",
+	"backgrounds/images/touhou03.jpg"
 ];
 
-/**
- * How many miliseconds for each announcement?
- * only works if enableAnnouncements = true
- */
-Config.announcementLength = 3000;
+/*Random background images order?*/
+var l_bgImagesRandom = false;
 
-/**
- * Image Filename
- * DROP IMAGE IN "images" FOLDER
- */
-Config.backgroundImage = "default1.jpg";
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
 
-/**
- * Enable debug messages?
- */
-Config.enableDebug = false;
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = false;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = false;
+
+/*Display actual song's name?*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/Lullaby of Deserted Hell.ogg", name: "Lullaby of Deserted Hell"},
+	{youtube: "j0PA0PX7lKo", name: "セプテットはディナーと共に"},
+	{youtube: "yHZfHPhNjXo", name: "いざオペラ座へ"},
+	{youtube: "-7VeTkLKdDw", name: "Departure"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 25;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Enjoy the server",
+"Thanks for joining us!"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
