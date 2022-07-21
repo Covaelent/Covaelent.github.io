671c588bc137504be8f311e69b0d0259c564f1c5
Covλlent <lunatsarratanai@gmail.com>
2022-07-21 07:28:59 +0100

    halo

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "Covalent: SCP-RP";
+var l_serverName = "Project Insurrection | Halo RP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
@@ -43,7 +43,7 @@ Avaiable options:
 "backgrounds/videos/yellowgreen.webm"
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/scp.webm";
+var l_background = "backgrounds/videos/halo.webm";
 
 /*(Image-background only)
 Place one or more images
