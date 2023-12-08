f187ddcc44b2151db39d88b2c91e7985459d22ed
Covaelent <xxxalucrd@gmail.com>
2023-12-08 10:04:29 +0000

    update

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ config.logo_use_image = false
 config.logo_image = ""
 
 // Text to be shown on logo
-config.logo_text = "Halo: Harvest"
+config.logo_text = "Halo: Reach"
 
 // Use the server's name for the logo
 config.logo_servername = false
