9d89f2db6cc148697e8e89b117593eaeaff3daa1
Covaelent <xxxalucrd@gmail.com>
2023-12-27 17:42:35 +0000

    .

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ config.logo_use_image = false
 config.logo_image = ""
 
 // Text to be shown on logo
-config.logo_text = "Fallout: New Vegas"
+config.logo_text = "Germainia"
 
 // Use the server's name for the logo
 config.logo_servername = false
@@ -50,7 +50,7 @@ config.background_use_video = true
 // You can add your own video backgrounds
 // Just make sure they are using webm VP8 format
 // (Place the video file in the backgrounds/videos folder)
-config.background_video = "vegas.webm"
+config.background_video = "ww2.webm"
 
 // (Image-background only)
 // Enable map-based background image?
