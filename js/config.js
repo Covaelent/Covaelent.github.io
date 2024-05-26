22ce1003645806ad477061d32d6453df052ece26
Covaelent <xxxalucrd@gmail.com>
2024-05-26 02:36:18 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -85,7 +85,7 @@ var l_musicDisplay = true;
 /*Music playlist
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
-	{youtube: "Jli-cviuC2c?si=IuVMYdpuckcg9x9x", name: ""},
+	{ogg: "songs/intro.ogg", name: "Lullaby of Madness"}
 ];
 
 /*Random music order?*/
