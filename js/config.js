35447c0654900558efabca707d5f9d734ee6d8f7
Lu <108835863+Covaelent@users.noreply.github.com>
2024-11-18 16:55:51 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,47 +1,115 @@
-// ignore
-var Config = {};
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.05
+*/
 
-/**
- * What should the text in the center of the screen be?
- * if empty it will fill in your Server Name
- */
-Config.title = " ";
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/logo.png";
 
-/**
- * Enable map text in the top left corner of the screen?
- */
-Config.enableMap = false;
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = " ";
 
-/**
- * Enable steamId text in the top right corner of the screen?
- */
-Config.enableSteamID = false;
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = true;
 
-/**
- * Enable announcements?
- */
-Config.enableAnnouncements = true;
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
 
-/**
- * What messages do you want to show up?
- * only works if enableAnnouncements = true
- */
-Config.announceMessages = [
-  "Our business is life itself."
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/umbrella.webm";
 
-/**
- * How many miliseconds for each announcement?
- * only works if enableAnnouncements = true
- */
-Config.announcementLength = 3000;
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/touhou01.jpg",
+	"backgrounds/images/touhou02.jpg",
+	"backgrounds/images/touhou03.jpg"
+];
 
-/**
- * Image Filename
- * DROP IMAGE IN "images" FOLDER
- */
-Config.backgroundImage = "images/umbrella.gif";
+/*Random background images order?*/
+var l_bgImagesRandom = false;
 
-/**
- * Enable debug messages?
- */
-Config.enableDebug = false;
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{youtube: "FnwMt7PJ-Yw", name: "NF - Lie"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 25;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Our business is Life itself."
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
