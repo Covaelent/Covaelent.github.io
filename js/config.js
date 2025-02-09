b5180f3af5ffd95783b59c4505ed4e1151baf26e
Lu <108835863+Covaelent@users.noreply.github.com>
2025-02-09 19:19:58 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -41,7 +41,7 @@ Config.announcementLength = 3000;
  * Image Filename
  * DROP IMAGE IN "images" FOLDER
  */
-Config.backgroundImage = "cwrp";
+Config.backgroundImage = "cwrp.jpg";
 
 /**
  * Enable debug messages?
