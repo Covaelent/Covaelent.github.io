fabd34dda6da44cd3253b60f4f972ad1a12c150e
Lu <108835863+Covaelent@users.noreply.github.com>
2025-05-13 22:01:15 +0100

    .

--- js/config.js
+++ js/config.js
@@ -120,7 +120,7 @@ var l_musicDisplay = false;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "site-10.ogg", name: "Unknown"},
+	{ogg: "hd2.ogg", name: "Unknown"},
 ];
 
 // Random music order?
