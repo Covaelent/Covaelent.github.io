fb19b234b0f1dbe52ca62cfeed0c826be98cc91e
Lu <108835863+Covaelent@users.noreply.github.com>
2025-05-13 18:26:23 +0100

    .

--- js/config.js
+++ js/config.js
@@ -55,7 +55,7 @@ var l_bgVideo = true;
 // Just make sure they are using webm VP8 format 
 // (Place the videos in the backgrounds/videos folder)
 // (You can also use external URL)
-var l_background = "site-10.webm";
+var l_background = "hd2.webm";
 
 // (Image-background only)
 // Enable map-based background image?
