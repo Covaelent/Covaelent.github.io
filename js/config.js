53af7fa4abefb9c685e4ab5b7b5c32f9cc4fba32
Lu <108835863+Covaelent@users.noreply.github.com>
2025-06-07 22:45:43 +0100

    .

--- js/config.js
+++ js/config.js
@@ -1,166 +1,48 @@
-//       _           _    _                     _ 
-//      | |         | |  | |                   | |
-//   ___| | ___  ___| | _| |     ___   __ _  __| |
-//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
-//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
-//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
-//     version 2.16
+// ignore
+var Config = {};
 
-// URL for the server logo
-// Leave it blank to use text instead
-// (Put the files in the images folder)
-// (You can also use external URL)
-var l_serverImage = "";
+/**
+ * What should the text in the center of the screen be?
+ * if empty it will fill in your Server Name
+ */
+Config.title = "Sundering of the Stars";
 
-// Center logo?
-// 'true' or 'false'
-var l_centerLogo = false;
+/**
+ * Enable map text in the top left corner of the screen?
+ */
+Config.enableMap = true;
 
-// Display current map and gamemode?
-// 'true' or 'false'
-var l_displayMapGamemode = false;
+/**
+ * Enable steamId text in the top right corner of the screen?
+ */
+Config.enableSteamID = true;
 
-// Custom server name configuration
-// Leave it blank for automatic server name
-var l_serverName = "Sundering of the Stars";
+/**
+ * Enable announcements?
+ */
+Config.enableAnnouncements = true;
 
-// Use a video for the background?
-// Otherwise it will use images
-// 'true' or 'false'
-var l_bgVideo = true;
-
-// Default available videos:
-// "aurora.webm"
-// "bluefall.webm"
-// "bluetilefloor.webm"
-// "bluewhitecircles.webm"
-// "bokehlens.webm"
-// "cleanbokeh.webm"
-// "coldnight.webm"
-// "colorfulburst.webm"
-// "dancingstars.webm"
-// "dropsflares.webm"
-// "focusorangebokeh.webm"
-// "glimmering.webm"
-// "glowingrings.webm"
-// "greencircles.webm"
-// "hues.webm"
-// "inthedeep.webm"
-// "modernflourish.webm"
-// "orangeaura.webm"
-// "warminside.webm"
-// "yellowgreen.webm"
-//
-// You can add your own video backgrounds
-// Just make sure they are using webm VP8 format 
-// (Place the videos in the backgrounds/videos folder)
-// (You can also use external URL)
-var l_background = "swrp.webm";
-
-// (Image-background only)
-// Enable map-based background image?
-// If enabled, the background will be an image based on the server current map
-// You need to have a background image with the same name as the map
-// Also, the image needs to be in .jpg format
-// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
-// 'true' or 'false'
-var l_bgImageMapBased = false;
-
-// (Image-background only)
-// Place one or more images
-// Multiple images will automatically shuffle between them
-// (Place the files in the backgrounds/images folder)
-// (You can also use external URL)
-var l_bgImages = [
-    "hero.jpg",
-	"hero1.jpg",
-	"hero2.jpg",
-	"hero3.jpg",
-	"hero4.jpg",
-	"hero5.jpg",
-	"hero6.jpg",
-	"hero7.jpg",
-	
+/**
+ * What messages do you want to show up?
+ * only works if enableAnnouncements = true
+ */
+Config.announceMessages = [
+  "Join our Community @ discord.gg/9DnnnH4q2F"
 ];
 
+/**
+ * How many miliseconds for each announcement?
+ * only works if enableAnnouncements = true
+ */
+Config.announcementLength = 3000;
 
-// (Image-background only)
-// Random background images order?
-// 'true' or 'false'
-var l_bgImagesRandom = false;
+/**
+ * Image Filename
+ * DROP IMAGE IN "images" FOLDER
+ */
+Config.backgroundImage = "default";
 
-// (Image-background only)
-// Delay between background images changes
-// in milliseconds
-var l_bgImageDuration = 7000;
-
-// (Image-background only)
-// Image background fade velocity
-// in milliseconds
-var l_bgImageFadeVelocity = 2000;
-
-// Enable background overlay?
-// 'true' or 'false'
-var l_bgOverlay = false;
-
-// Background darkening amount
-// 0 for none (0%), 100 for pitch black (100%)
-var l_bgDarkening = 20;
-
-// Play music during the loading screen?
-// 'true' or 'false'
-var l_music = false;
-
-// Display current music name?
-// 'true' or 'false'
-var l_musicDisplay = false;
-
-// Music playlist
-// Add as many youtube ids/urls or ogg files you want
-// (Place the ogg files in the music folder)
-// (You can also use external URL for the ogg files)
-var l_musicPlaylist = [
-	{ogg: "swrp.ogg", name: "Unknown"},
-];
-
-// Random music order?
-// 'true' or 'false'
-var l_musicRandom = false;
-
-// Music volume
-// Choose a value between 0 (quietest) and 100 (loudest)
-var l_musicVolume = 27;
-
-// Enable custom messages?
-// 'true' or 'false'
-var l_messagesEnabled = false;
-
-// Enter your custom messages below
-var l_messages = [
-];
-
-// Random message order?
-// 'true' or 'false'
-var l_messagesRandom = false;
-
-// Delay between message changes
-// in milliseconds
-var l_messagesDelay = 5000;
-
-// Messages fade time
-// in milliseconds
-var l_messagesFade = 1000;
-
-// Show errors inside Garry's Mod?
-// Can be useful for finding errors
-// 'true' or 'false'
-var l_showErrorsIngame = false;
-
-//==============================================================
-//========================== WARNING ===========================
-//==============================================================
-//============ Do not edit anything below this line ============
-//==============================================================
-var checkConfigFile = function() {
-	return true;
-};
+/**
+ * Enable debug messages?
+ */
+Config.enableDebug = false;
